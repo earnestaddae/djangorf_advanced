07f331b3a7fcd20d105c492b5c3d08c27378e611
Ernest Addae <earnestaddae@gmail.com>
2022-06-09 16:03:43 +0800

    Fix patch error

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,6 +1,7 @@
 import pytest
-import mock
 from psycopg2 import OperationalError as Psycopg2Error
+
+from unittest.mock import patch
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -10,25 +11,21 @@ pytestmark = pytest.mark.django_db
 
 class TestCommand:
 
-    @pytest.mark.skip
-    def test_wait_for_db_ready(self, mocker):
-        mk = mocker.Mock()
-        mk.patch('core.management.commands.wait_for_db.Command.check')
-        mk.return_value = True
+    @patch('core.management.commands.wait_for_db.Command.check')
+    def test_wait_for_db_ready(self, patched_check):
+        patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        mk.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
-
-    @pytest.mark.skip
-    @mock.patch('time.sleep')
-    def test_wait_for_db_delay(self, mocked_sleep, mocker):
-        mk = mocker.Mock()
-        mk.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+    @patch('core.management.commands.wait_for_db.Command.check')
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
-        assert mk.call_count == 6
+        assert patched_check.call_count == 6
 
-        mk.assert_called_with(databases=['default'])
+        patched_check.assert_called_with(databases=['default'])
